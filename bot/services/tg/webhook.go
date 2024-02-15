d73e6afb0dcd090e42c7421e7cd0cb8ccf412077
lohopupa <lohopupa@gmail.com>
2024-02-16 03:56:08 +0600

    testing docker args

--- bot/services/tg/webhook.go
+++ bot/services/tg/webhook.go
@@ -28,14 +28,11 @@ func CreateWH(config config.TgConfig) (*WebHookBot, error) {
 
 	logger.Info("Authorized on account %s", bot.Self.UserName)
 
-	sertFile := config.WebHookCertFolder + "/" + "cert.pem"
-	keyFile := config.WebHookCertFolder + "/" + "key.pem"
-
-	go http.ListenAndServeTLS("localhost:8443", sertFile, keyFile, nil)
+	go http.ListenAndServeTLS("localhost:8443", "cert.pem", "key.pem", nil)
 	time.Sleep(500 * time.Millisecond)
 
 	certFile := requestFileData{
-		FileName: sertFile,
+		FileName: "cert.pem",
 	}
 
 	wh, err := tgbotapi.NewWebhookWithCert("http://tg-webhook.lhpa.ru/"+bot.Token, certFile)
